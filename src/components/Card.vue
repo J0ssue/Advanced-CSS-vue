98be260125c726df1a6a7781623c9669a6add653
Josue Diaz <jdiaz77711@gmail.com>
2019-09-03 11:51:31 +0100

    add tours section && tours section styling && add card component && add card component styling and functionality

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,30 +1,66 @@
 <template>
 	<div class="card">
 		<div class="card__side card__side--front">
-			<div class="card__picture card__picture--1">
+			<div
+				class="card__picture"
+				:class="{
+					'card__picture--1': index === 0,
+					'card__picture--2': index === 1,
+					'card__picture--3': index === 2
+				}"
+			>
 				&nbsp;
 			</div>
 			<h4 class="card__heading">
-				<span class="card__heading-span card__heading-span--1"
-					>the sea explorer</span
-				>
+				<span
+					class="card__heading-span"
+					:class="{
+						'card__heading-span--1': index === 0,
+						'card__heading-span--2': index === 1,
+						'card__heading-span--3': index === 2
+					}"
+					v-text="card.title"
+				></span>
 			</h4>
 			<div class="card__details">
 				<ul>
-					<li>3 day tour</li>
-					<li>Up to 30 people</li>
-					<li>2 tour guides</li>
-					<li>Sleep in cozy hotels</li>
-					<li>Difficulty: easy</li>
+					<li
+						v-for="(detail, index) in card.details"
+						:key="index"
+						v-text="detail"
+					></li>
 				</ul>
 			</div>
 		</div>
-		<div class="card__side card__side--back card__side--back-1"></div>
+		<div
+			class="card__side card__side--back card__side--back-1"
+			:class="{
+				'card__side--back-1': index === 0,
+				'card__side--back-2': index === 1,
+				'card__side--back-3': index === 2
+			}"
+		>
+			<div class="card__cta">
+				<div class="card__price-box">
+					<p class="card__price-only">Only</p>
+					<div class="card__price-value" v-text="card.price"></div>
+				</div>
+				<a href="#" class="btn btn--white">book now!</a>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 	export default {
+		props: {
+			card: {
+				type: Object
+			},
+			index: {
+				type: Number
+			}
+		},
 		name: "Card"
 	};
 </script>
--- src/sections/home/Tours.vue
+++ src/sections/home/Tours.vue
@@ -1,18 +1,60 @@
 <template>
 	<section class="section-tours">
-		<header class="u-center-text">
+		<header class="u-center-text u-margin-bottom-big u-margin-top-big">
 			<heading-secondary text="Most popular tours"></heading-secondary>
 		</header>
 		<div class="row">
-			<div class="col-1-of-3"><card></card></div>
-			<div class="col-1-of-3">2</div>
-			<div class="col-1-of-3">3</div>
+			<div class="col-1-of-3" v-for="(card, index) in cards" :key="card.price">
+				<card :card="card" :index="index"></card>
+			</div>
+		</div>
+		<div class="u-center-text u-margin-top-huge">
+			<a href="#" class="btn btn--green">Discover all tours</a>
 		</div>
 	</section>
 </template>
 
 <script>
 	export default {
-		name: "ToursSection"
+		name: "ToursSection",
+		data() {
+			return {
+				cards: [
+					{
+						title: "the sea explorer",
+						details: [
+							"3 day tour",
+							"Up to 30 people",
+							"2 tour guides",
+							"Sleep in cozy hotels",
+							"Difficulty: easy"
+						],
+						price: "€297"
+					},
+					{
+						title: "the forest hiker",
+						details: [
+							"7 day tour",
+							"Up to 40 people",
+							"6 tour guides",
+							"Sleep in provided tent",
+							"Difficulty: medium"
+						],
+						price: "€497"
+					},
+					{
+						title: "the snow adventurer",
+						details: [
+							"5 day tour",
+							"Up to 15 people",
+							"3 tour guides",
+							"Sleep in provided tents",
+							"Difficulty: hard"
+						],
+						price: "€897"
+					}
+				]
+			};
+		}
 	};
 </script>
