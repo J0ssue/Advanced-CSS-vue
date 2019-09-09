d86b7fe09b2b93296de7d18bbb50a60196523978
Josue Diaz <jdiaz77711@gmail.com>
2019-09-09 09:34:03 +0100

    previously done

--- src/components/Story.vue
+++ src/components/Story.vue
@@ -1,29 +1,22 @@
 <template>
 	<div class="story">
 		<figure class="story__shape">
-			<img
-				src="../assets/images/nat-8.jpg"
-				alt="person on a tour"
-				class="story__img"
-			/>
+			<img :src="img" alt="person on a tour" class="story__img" />
+			<figcaption class="story__caption" v-text="name"></figcaption>
 		</figure>
 		<div class="story__text">
 			<heading-tertiary
 				class="u-margin-bottom-small"
-				text="I had the best week ever with my family"
+				:text="title"
 			></heading-tertiary>
-			<p>
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, saepe.
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
-				similique! Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
-				magni repellendus et in consequuntur necessitatibus, ipsam odit minima
-			</p>
+			<p v-text="text"></p>
 		</div>
 	</div>
 </template>
 
 <script>
 	export default {
+		props: ["img", "name", "title", "text"],
 		name: "Story"
 	};
 </script>
--- src/sections/home/Stories.vue
+++ src/sections/home/Stories.vue
@@ -5,14 +5,46 @@
 				text="We make people genuinely happy"
 			></heading-secondary>
 		</header>
-		<div class="row">
-			<story />
+		<div class="row" v-for="story in stories" :key="story.name">
+			<story
+				:img="story.img"
+				:name="story.name"
+				:title="story.title"
+				:text="story.text"
+			/>
+		</div>
+		<div class="u-center-text u-margin-bottom-huge">
+			<a href="#" class="btn-text">Read all stories &rarr;</a>
 		</div>
 	</section>
 </template>
 
 <script>
 	export default {
-		name: "StoriesSection"
+		name: "StoriesSection",
+		data() {
+			return {
+				stories: [
+					{
+						img: require("../../assets/images/nat-8.jpg"),
+						name: "Mary Smith",
+						title: "I had the best week ever with my family",
+						text: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, saepe.
+																									Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
+																									similique! Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+																									magni repellendus et in consequuntur necessitatibus, ipsam odit minima`
+					},
+					{
+						img: require("../../assets/images/nat-8.jpg"),
+						name: "Jack Wilson",
+						title: "Wow my life is completely different now",
+						text: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, saepe.
+																				Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
+																									similique! Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+																									magni repellendus et in consequuntur necessitatibus, ipsam odit minima`
+					}
+				]
+			};
+		}
 	};
 </script>
